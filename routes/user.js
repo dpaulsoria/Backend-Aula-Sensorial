2f7720b2a3a2c04cb02dde9857c5d04fa42b245b
Paul Soria <dpaulsoria@gmail.com>
2023-11-06 19:51:00 -0500

    Doc Swagger ejemplo

--- routes/user.js
+++ routes/user.js
@@ -7,12 +7,75 @@ const db = require('../models').USER;
  * @swagger
  * /user:
  *  get:
- *    summary: Obtiene una lista de los usuarios.
- *    description: Obtiene una lista de los usuarios.
- *    response:
+ *    summary: Obtiene una lista de todos los usuarios.
+ *    description: Metodo findAll sobre la tabla de usuarios.
+ *    parameters:
+ *      - in: query
+ *        name: page
+ *        schema:
+ *          type: integer
+ *          default: 1
+ *        description: Número de página para la paginación
+ *      - in: query
+ *        name: limit
+ *        schema:
+ *          type: integer
+ *          default: 10
+ *        description: Número de usuarios por página
+ *      - in: header
+ *        name: X-Request-ID
+ *        schema:
+ *          type: string
+ *          format: uuid
+ *        required: false
+ *        description: Identificador único del request
+ *    responses:
  *      200:
  *        description: Una lista de usuarios.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                users:
+ *                  type: array
+ *                  items:
+ *                    $ref: '#/components/schemas/User'
+ *                page:
+ *                  type: integer
+ *                limit:
+ *                  type: integer
+ *                totalPages:
+ *                  type: integer
+ *                totalCount:
+ *                  type: integer
+ *            examples:
+ *              application/json: 
+ *                - page: 1
+ *                  limit: 10
+ *                  totalPages: 5
+ *                  totalCount: 50
+ *                  users: [{id: 1, name: "John Doe", email: "john@example.com"}, {...}]
+ *      400:
+ *        description: Entrada inválida
+ *      500:
+ *        description: Error en el servidor
+ * 
+ * components:
+ *  schemas:
+ *    User:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: integer
+ *          format: int64
+ *        name:
+ *          type: string
+ *        email:
+ *          type: string
+ *          format: email
  */
+
 router.get('/', function (req, res, next) {
   let users = db.findAll();
   res.json({
