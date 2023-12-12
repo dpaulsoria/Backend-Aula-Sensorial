cc4dd78d73e0a1751688ab2b28b4a125320894c1
Reynaldo Olvera <46010629+clreolve@users.noreply.github.com>
2023-12-12 02:47:11 +0000

    despligue3

--- config/sequelize-config.js
+++ config/sequelize-config.js
@@ -3,18 +3,28 @@ const { Sequelize } = require("sequelize");
 
 const env = process.env.NODE_ENV || "production";
 
-let sequelize;
+// Configuración de la base de datos
+const sequelize = new Sequelize({
+  dialect: 'sqlite', // Especifica el dialecto como SQLite
+  storage: 'database.sqlite', // Ruta donde se guardará la base de datos SQLite
+});
 
-if (process.env.CONECTION_STRING !== "") {
-  sequelize = new Sequelize(process.env.CONECTION_STRING, {
-    dialect: "postgres",
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  });
-} else {
-  sequelize = new Sequelize(
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Conexión establecida correctamente.");
+    
+    await sequelize.sync({ force: true });
+    console.log(
+      "Modelos sincronizados correctamente (la base de datos se creó si no existía)."
+    );
+
+  } catch (error) {
+    console.error("Error al conectar y sincronizar la base de datos:", error);
+  }
+})();
+
+/* const sequelize = new Sequelize(
     process.env.DB_NAME || "nombre_base_datos",
     process.env.DB_USER || "usuario",
     process.env.DB_PASS || "contraseña",
@@ -22,7 +32,6 @@ if (process.env.CONECTION_STRING !== "") {
       host: process.env.DB_HOST || "localhost",
       dialect: "postgres",
     }
-  );
-}
+  ); */
 
 module.exports = sequelize;
